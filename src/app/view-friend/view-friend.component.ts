80d74db6adb66513bb36bac6effaf279c10db739
HarshaSalih <harshasalih2000@gmail.com>
2022-11-30 17:10:25 +0530

    commits

---
+++ src/app/view-friend/view-friend.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-friend',
+  templateUrl: './view-friend.component.html',
+  styleUrls: ['./view-friend.component.css']
+})
+export class ViewFriendComponent {
+
+  friends:any = []
+
+}
